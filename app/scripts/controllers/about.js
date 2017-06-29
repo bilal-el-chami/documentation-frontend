305fb9c87985c8ce68c525887ffc796a2e12c009
Bilal EL CHAMI <belchami@algofi.fr>
2017-06-29 14:15:40 +0200

    Adding Files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name documentationFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the documentationFrontendApp
+ */
+angular.module('documentationFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name documentationFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the documentationFrontendApp
+ */
+angular.module('documentationFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
